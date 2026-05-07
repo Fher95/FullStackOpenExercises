91e05f6f3bc8702a1dae0f7d94eaf2b5801efa4d
Fernando Gómez <luernandogg@gmail.com>
2026-05-07 12:42:06 -0500

    ESLint installed into devDependencies

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,59 +1,54 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const app = express()
 const Person = require('./modules/person')
-const morgan = require('morgan');
+const morgan = require('morgan')
 morgan.token('body', function getBody(req) {
-  return JSON.stringify(req.body);
+  return JSON.stringify(req.body)
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(express.json())
-app.use(express.static('dist'));
+app.use(express.static('dist'))
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(result => {
-    response.json(result);
-  });
+    response.json(result)
+  })
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-  const id = request.params.id;
+  const id = request.params.id
   Person.findById(id)
     .then(result => {
-      console.log('RESULT ID: ', result)
       if (result) {
-        response.json(result);
+        response.json(result)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
-    })
-    .catch(error => next(error));
-})
-
-app.get('/api/info', (request, response) => {
-  const date = new Date();
-  Person.countDocuments({})
-    .then(count => {
-      response.send(`<div> <p>Phonebook has info for ${count}</p> <p>${date.toString()}</p></div>`)
-    });
-})
-
-app.delete('/api/persons/:id', (request, response, next) => {
-  const id = request.params.id;
-  Person.findByIdAndDelete(id)
-    .then(result => {
-      // I send the deleted object for info handling in the frontend
-      response.json(result);
     })
     .catch(error => next(error))
 })
 
-function generateId() {
-  return String(Math.floor(Math.random() * 9999));
-}
+app.get('/api/info', (request, response) => {
+  const date = new Date()
+  Person.countDocuments({})
+    .then(count => {
+      response.send(`<div> <p>Phonebook has info for ${count}</p> <p>${date.toString()}</p></div>`)
+    })
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
+  Person.findByIdAndDelete(id)
+    .then(result => {
+      // I send the deleted object for info handling in the frontend
+      response.json(result)
+    })
+    .catch(error => next(error))
+})
 
 app.post('/api/persons', (request, response, next) => {
-  let newPerson = { ...request.body };
+  let newPerson = { ...request.body }
   if (!newPerson.name || !newPerson.number) {
     return response.status(400).json({ error: 'name and number are required' })
   }
@@ -61,27 +56,27 @@ app.post('/api/persons', (request, response, next) => {
     .then(result => {
       if (result?.length === 0) {
         Person.create(newPerson).then(result => {
-          response.json(result);
+          response.json(result)
         })
-          .catch(error => next(error));
+          .catch(error => next(error))
       } else {
-        response.status(400).json({ error: 'name must be unique' });
+        response.status(400).json({ error: 'name must be unique' })
       }
     })
-    .catch(error => next(error));
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const person = { ...request.body };
+  const person = { ...request.body }
   Person.findByIdAndUpdate(request.params.id, person, { returnDocument: 'after', runValidators: true })
     .then(result => {
       if (result) {
-        return response.json(result);
+        return response.json(result)
       }
-      return response.status(404).end();
+      return response.status(404).end()
     })
     .catch(error => next(error))
-});
+})
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
--- part3/phonebook-backend/modules/person.js
+++ part3/phonebook-backend/modules/person.js
@@ -1,41 +1,41 @@
-const mongoose = require('mongoose');
-mongoose.set('strictQuery', false);
-const url = process.env.MONGODB_URI;
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
+const url = process.env.MONGODB_URI
 
 mongoose.connect(url, { family: 4 })
-    .then(result => {
-        console.log('Connected to MongoDB');
-    })
-    .catch(error => {
-        console.log('Error connecting to MongoDB: ', error.message);
-    });
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('Error connecting to MongoDB: ', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: (v) => {
+        return /^\d{2,3}-\d+$/.test(v)
+      },
+      message: props => `${props.value} is not a valit number phone.`
     },
-    number: {
-        type: String,
-        validate: {
-            validator: (v) => {
-                return /^\d{2,3}-\d+$/.test(v);
-            },
-            message: props => `${props.value} is not a valit number phone.`
-        },
-        required: true
-    },
-});
+    required: true
+  },
+})
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString();
-        delete returnedObject._id;
-        delete returnedObject.__v;
-    }
-});
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
-const Person = mongoose.model('Person', personSchema);
+const Person = mongoose.model('Person', personSchema)
 
-module.exports = Person;
+module.exports = Person
