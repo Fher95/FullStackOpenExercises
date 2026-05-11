9a4ff9b6aefe214c09b933ef5e3f978a9675826c
Fernando Gómez <luernandogg@gmail.com>
2026-05-11 13:31:00 -0500

    Exercises 4.3-4.7

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,4 +1,6 @@
-const dummy = (blogs) => {
+const _ = require('lodash')
+
+const dummy = () => {
   return 1
 }
 
@@ -7,11 +9,36 @@ const totalLikes = (blogs) => {
 }
 
 const favoriteBlog = (blogs) => {
-  return blogs.sort((a, b) => -(a.likes - b.likes))[0]
+  return [...blogs].sort((a, b) => -(a.likes - b.likes))[0]
+}
+
+const mostBlogs = (blogs) => {
+  let result = { blogs: 0 }
+  const blogsByAuthor = _.groupBy(blogs, 'author')
+  Object.keys(blogsByAuthor).forEach((author) => {
+    if (blogsByAuthor[author].length > result.blogs) {
+      result = { author, blogs: blogsByAuthor[author].length }
+    }
+  })
+  return result
+}
+
+const mostLikes = (blogs) => {
+  let result = { likes: 0 }
+  const blogsByAuthor = _.groupBy(blogs, 'author')
+  Object.keys(blogsByAuthor).forEach(author => {
+    const totalLikes = blogsByAuthor[author].reduce((total, item) => total + item.likes, 0)
+    if (totalLikes > result.likes) {
+      result = { author, likes: totalLikes }
+    }
+  })
+  return result
 }
 
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
 }
